778f5d849d4a2248c960eb83a690a3a3bc4d5c0b
joanlojo <joanlojo.7@gmail.com>
2017-11-28 01:22:11 +0100

    poca cosa

--- GincanaEnti/app/src/main/java/com/enti/joan/gincanaenti/FragmentPista.java
+++ GincanaEnti/app/src/main/java/com/enti/joan/gincanaenti/FragmentPista.java
@@ -1,10 +1,25 @@
 package com.enti.joan.gincanaenti;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by jlopez on 11/27/2017.
  */
 
 public class FragmentPista extends Fragment {
+    private View vista;
+    public View onCreateView(LayoutInflater inflater, ViewGroup contenedor, Bundle savedInstanceState){
+        vista = inflater.inflate(R.layout.activity_show_actual_pista, contenedor, false);
+        //codigo para inicializar lo que el fragment tenga q mostrar, como un recycler view
+        return vista;
+    }
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+        //codigo para una vez creada la activdad que contiene el fragmento ahcer lo q sea
+    }
 }
