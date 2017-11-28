a60bcdc93efa24032003f95e7bb9854cf1e9d0ac
joanlojo <joanlojo.7@gmail.com>
2017-11-29 00:05:19 +0100

    mapa sin funcionar

--- GincanaEnti/app/src/main/java/com/enti/joan/gincanaenti/show_actual_pista.java
+++ GincanaEnti/app/src/main/java/com/enti/joan/gincanaenti/show_actual_pista.java
@@ -1,26 +1,45 @@
 package com.enti.joan.gincanaenti;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class show_actual_pista extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 
-    private Button btn_salir;
-    @Override
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+public class show_actual_pista extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener, OnMapReadyCallback {
+    private GoogleApiClient apiClient;
+    private GoogleMap mapa;
+
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        apiClient = new GoogleApiClient(this).addConnectionCallbacks(this).addApi(LocationServices.API).build();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        FragmentMapa fragmentMapa=(FragmentMapa)getFragmentManager().findFragmentById(R.id.FragmentMapa);
+        if(fragmentMapa== null)
+            Toast.makeText(this, "Mapa no encontrado", Toast.LENGTH_SHORT).show();
+        fragmentMapa.getMapAsync(this);
     }
 
+    public void onMapReady(GoogleMap p){
+        mapa = p;
+    }
 }
