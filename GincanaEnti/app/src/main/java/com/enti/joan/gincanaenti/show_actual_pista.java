8424f790ee61830acf91a0a19a49efa0032fa024
joanlojo <joanlojo.7@gmail.com>
2017-11-29 19:46:49 +0100

    muestr pista bien

--- GincanaEnti/app/src/main/java/com/enti/joan/gincanaenti/show_actual_pista.java
+++ GincanaEnti/app/src/main/java/com/enti/joan/gincanaenti/show_actual_pista.java
@@ -3,16 +3,21 @@ package com.enti.joan.gincanaenti;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -27,17 +32,17 @@ import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.concurrent.TimeUnit;
 
-/*public class show_actual_pista extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener, OnMapReadyCallback {
+public class show_actual_pista extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener, OnMapReadyCallback {
     private GoogleApiClient apiClient;
     private GoogleMap mapa;
 
     protected void onCreate(Bundle savedInstanceState) {
         apiClient = new GoogleApiClient(this).addConnectionCallbacks(this).addApi(LocationServices.API).build();
 
-        FragmentMapa fragmentMapa=(FragmentMapa)getFragmentManager().findFragmentById(R.id.FragmentMapa);
+       /* FragmentMapa fragmentMapa=(FragmentMapa)getFragmentManager().findFragmentById(R.id.FragmentMapa);
         if(fragmentMapa== null)
-            Toast.makeText(this, "Mapa no encontrado", Toast.LENGTH_SHORT).show();
-        fragmentMapa.getMapAsync(this);
+            Toast.makeText(this, "Mapa no encontrado", Toast.LENGTH_SHORT).show();*/
+       // fragmentMapa.getMapAsync(this);
     }
 
     public void onMapReady(GoogleMap p){
@@ -46,20 +51,47 @@ import java.util.concurrent.TimeUnit;
 
     public void onLocationChanged(Location location) {
     }
-    }
+
 
     public void onConnectionFailed(ConnectionResult result) {
     }
 
-}*/
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    }
 
-public class show_actual_pista extends AppCompatActivity{
+    public void onConnectionSuspended(int i) {
+    }
 
+    public void onStatusChanged(String proveedor, int estado, Bundle extras) {
+    }
+
+    public void onProviderDisabled(String proveedor) {
+    }
+    public void onConnected(@Nullable Bundle bundle) {
+
+
+    }
+}
+
+/*public class show_actual_pista extends AppCompatActivity{
+    private RecyclerView recyclerView;
+    public Adaptador adaptador;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_actual_pista);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-    }}
+        TextView des = (TextView)findViewById(R.id.des);
+        TextView id = (TextView)findViewById(R.id.id);
+        TextView idNext = (TextView)findViewById(R.id.idNext);
+        TextView latitud = (TextView)findViewById(R.id.latitud);
+        TextView longitud = (TextView)findViewById(R.id.longitud);
+        ImageView imagen = (ImageView)findViewById(R.id.imageView);
+        des.setText("Descripcion: " + listaPistas.obtenerP(0).getDescripcion().toString());
+        id.setText("ID: " + listaPistas.obtenerP(0).getId().toString());
+        idNext.setText("ID Next: " + listaPistas.obtenerP(0).getIdNext().toString());
+        latitud.setText("Lat: " + Double.toString(listaPistas.obtenerP(0).getLatitud()));
+        longitud.setText("Long: " + Double.toString(listaPistas.obtenerP(0).getLongitud()));
+        int k=listaPistas.obtenerP(0).getTipo();
+        imagen.setImageResource(k);
+    }}*/
